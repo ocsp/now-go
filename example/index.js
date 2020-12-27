1bca707f1b0a1534611be7ffb603b960b4eb35ce
OCSP <58361304+ocsp@users.noreply.github.com>
2020-12-27 13:10:45 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,14 +1,14 @@
 const go = require('..')
 
 const config = {
-  '/': 'https://github.com/amio/now-go',
+  '/': 'Hi',
   '/tag': 'Now go, let the legend come back to life!',
   '/hero': 'https://amio.github.io/now-go/ocelot.jpg',
   '*': (req) => {
     if (req.headers.host === 'localhost:3000') {
       return 'Hello from localhost:3000'
     } else {
-      return 'Yet another tinyurl service.'
+      return 'Unknown request'
     }
   }
 }

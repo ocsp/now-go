1d8dca437bed972ab34745739be6ccc4b0fcb3a8
OCSP <58361304+ocsp@users.noreply.github.com>
2020-12-27 13:18:23 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,14 +1,12 @@
 const go = require('..')
 
 const config = {
-  '/': 'Hi',
-  '/tag': 'Now go, let the legend come back to life!',
-  '/hero': 'https://amio.github.io/now-go/ocelot.jpg',
+  '/': 'OpenCache Server 0.1.1',
   '*': (req) => {
     if (req.headers.host === 'localhost:3000') {
       return 'Hello from localhost:3000'
     } else {
-      return 'Unknown request'
+      return 'MISS from Origin-HND1'
     }
   }
 }
